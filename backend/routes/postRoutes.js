c5e34ace9819aa414ac00e8566c55a0f70eb080e
Alexander <lexwell7@yahoo.com>
2023-11-27 15:39:37 -0600

    route change

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -8,7 +8,7 @@ router.get("/", userProtect, getAllPosts)
 router.post("/create-post",  makePost)  
 router.get("/:id",  getUserPost)
 router.put("/updatepost/:id", updatePost)
-router.delete("/:postId",userProtect,  deletePost )
+router.delete("/:postId", deletePost )
 router.put('/like/:postId', likePost);
 router.put('/editing/:postId',userProtect,  changeEditing);
 router.get("/search/:location",  getPostByLocation)
